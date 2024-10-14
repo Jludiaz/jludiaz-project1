f4d79d86b7f50b5a8ff57e122f908976ee608f43
Jeremy <jeremyludiaz@gmail.com>
2024-10-14 19:37:40 -0400

    Defined Methods

--- Station.java
+++ Station.java
@@ -1,3 +1,34 @@
 public class Station {
+    private Station nextStation;
+    private Station prevStation;
     
+
+    public Station(String station, String location){
+
+    }
+
+    public void addNext(Station station){
+
+    }
+
+    public void addPrev(Station station){
+
+    }
+
+    public Boolean isAvailable(){
+
+    }
+
+    public void connect(Station station){
+        
+    }
+
+
+
+    public String toString(){
+        return "";
+    }
+
+
+
 }
--- TransferStation.java
+++ TransferStation.java
@@ -1,3 +1,14 @@
 public class TransferStation {
-    
+
+    public TransferStation(String color, String location){
+
+    }
+
+    public void addTransferStationNext(Station station){
+
+    }
+
+    public void addTransferStationPrev(Station station){
+
+    }
 }
